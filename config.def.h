c37cc0cadae842b0a3c5d1707789fe61be50c74a
yljos <yljos@outlook.com>
2026-02-16 13:58:31 +0000

    fcitx5-remote -t

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *fcitx5cmd[]  = { "/home/huai/.config/dwm/ime.sh", NULL };
+static const char *fcitx5cmd[]  = { "fcitx5-remote","-t", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
