18a536e1460f82aba36c5fd841fa2f63c0bb8aa6
yljos <yljos@outlook.com>
2026-02-17 02:18:56 +0000

    alacritty

--- config.def.h
+++ config.def.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	{ "firefox-esr",  NULL,    NULL,       1 << 0,       0,           -1 },
         { "xfreerdp",  NULL,       NULL,       1 << 2,       0,           -1 },
         { "steam",  NULL,       NULL,       1 << 4,       0,           -1 },
-        { "kitty",  NULL,       NULL,       1 << 3,       0,           -1 },
+        { "alacritty",  NULL,       NULL,       1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *fcitx5cmd[]  = { "fcitx5-remote","-t", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
